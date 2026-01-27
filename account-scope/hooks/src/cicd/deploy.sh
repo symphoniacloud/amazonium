1248edbb0f476d707c0f310bb0118d535b60cdd3
Mike Roberts <mike@symphonia.io>
2026-01-27 10:57:16 -0500

    CICD for hooks

---
+++ account-scope/hooks/src/cicd/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+STACK_NAME="${STACK_NAME:-amazonium-hooks-codebuild}"
+
+echo "Deploying CodeBuild stack $STACK_NAME"
+
+aws cloudformation deploy \
+  --stack-name "$STACK_NAME" \
+  --template-file codebuild-template.yaml \
+  --capabilities CAPABILITY_IAM \
+  --no-fail-on-empty-changeset
